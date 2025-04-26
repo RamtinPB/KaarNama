8060a17ab42f69efe1b6139dc8edb84545fbf37e
RamtinPB <ramtin.panahibazaz@gmail.com>
2025-04-26 17:09:00 +0330

    Kaar Nama v2

--- src/components/CalendarBelt.tsx
+++ src/components/CalendarBelt.tsx
@@ -175,21 +175,21 @@ export default function CalendarBelt({ onSelectDays }: CalendarBeltProps) {
 			</div>
 			<div className="p-4 my-4 w-screen flex flex-row gap-4 items-center justify-center">
 				<span>از</span>
-				<div className="relative">
+				<div className="flex gap-1">
 					{/* @ts-ignore */}
 					<Input
 						type="text"
 						placeholder="YYYY-MM-DD"
 						value={rangeFrom}
 						onChange={(e) => handleDateChange(e, setRangeFrom)}
-						className="border p-3 rounded"
+						className="border p-2 rounded"
 					/>
 					{/* @ts-ignore */}
 					<IconButton
 						onClick={() => {
 							setRangeFrom(today.format("YYYY-MM-DD"));
 						}}
-						className="bg-blue-500 text-white p-3 !absolute left-0 top-0 rounded"
+						className="bg-blue-500 !p-2 text-white  rounded"
 					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -197,11 +197,11 @@ export default function CalendarBelt({ onSelectDays }: CalendarBeltProps) {
 							viewBox="0 0 24 24"
 							strokeWidth="1.5"
 							stroke="currentColor"
-							className="size-6"
+							className="size-4"
 						>
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
 							/>
 						</svg>
